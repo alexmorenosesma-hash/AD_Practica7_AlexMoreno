5ba6620dc0d662e37237d63fbd8292aa80e36814
alexm <alex.moreno.sesma@iestubalcain.net>
2026-01-17 13:15:15 +0100

    Initial commit

---
+++ src/main/java/es/alexmoreno/AD_Practica7_AlexMoreno/AdPractica7AlexMorenoApplication.java
@@ -0,0 +1,13 @@
+package es.alexmoreno.AD_Practica7_AlexMoreno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdPractica7AlexMorenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdPractica7AlexMorenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/alexmoreno/AD_Practica7_AlexMoreno/AdPractica7AlexMorenoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.alexmoreno.AD_Practica7_AlexMoreno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdPractica7AlexMorenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
