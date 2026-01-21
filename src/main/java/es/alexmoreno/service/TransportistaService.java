ec7f990884a2da5db50d991106336f17019eec6f
alexm <alex.moreno.sesma@iestubalcain.net>
2026-01-21 12:30:16 +0100

    Controller transportista y algun cambion en el service

--- src/main/java/es/alexmoreno/service/TransportistaService.java
+++ src/main/java/es/alexmoreno/service/TransportistaService.java
@@ -1,11 +1,16 @@
 package es.alexmoreno.service;
 
 import es.alexmoreno.domain.Rol;
+import es.alexmoreno.domain.Ruta;
 import es.alexmoreno.domain.Transportista;
+import es.alexmoreno.domain.Transportista_Ruta;
 import es.alexmoreno.domain.Usuario;
+import es.alexmoreno.domain.Vehiculo;
 import es.alexmoreno.repository.TransportistaRepository;
 import es.alexmoreno.security.UserContext;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -45,7 +50,64 @@ public class TransportistaService {
             original.setUsuario(transportista.getUsuario());
             return transportistaRepository.save(original);
         }else{
-            throw new RuntimeException("No tienes permisos para crear transportistas");
+            throw new RuntimeException("No tienes permisos para modificar transportistas");
+        }
+    }
+    
+    @Transactional
+    public void eliminarTransportista(Long id){
+        Usuario currentUser=userContext.getCurrentUser();
+        
+        if (currentUser.getRoles().contains(Rol.Admin)){
+            transportistaRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("No tienes permisos para eliminar transportistas");
+        }
+    }
+    
+    public List<Transportista> verDatos(){
+        Usuario currentUser=userContext.getCurrentUser();
+        if (currentUser.getRoles().contains(Rol.Admin)){
+            return listarTodos();
+        }else{
+            List<Transportista> lista=new ArrayList<>();
+            lista.add(currentUser.getTransportista());
+            return lista;
+        }
+    }
+    
+    @Transactional
+    public Transportista asignarVehiculo(long id,Vehiculo vehiculo){
+        Usuario currentUser=userContext.getCurrentUser();
+        
+        if(currentUser.getRoles().contains(Rol.Admin)){
+            Transportista transportista=buscarId(id);
+            transportista.setVehiculo(vehiculo);
+            return transportistaRepository.save(transportista);
+        }else{
+            throw new RuntimeException("No tienes permisos para asignar vehiculos a transportistas");
+        }
+    }
+    
+    @Transactional
+    public void eliminarVehiculo(long id){
+        Usuario currentUser=userContext.getCurrentUser();
+        
+        if(currentUser.getRoles().contains(Rol.Admin)){
+            Transportista transportista=buscarId(id);
+            transportista.setVehiculo(null);
+            transportistaRepository.save(transportista);
+        }else{
+            throw new RuntimeException("No tienes permisos para eliminar vehiculos a transportistas");
+        }
+    }
+    
+    public List<Transportista> listarTodos(){
+        Usuario currentUser=userContext.getCurrentUser();
+        if (currentUser.getRoles().contains(Rol.Admin)){
+            return transportistaRepository.findAll();
+        }else{
+            throw new RuntimeException("No tienes permisos para ver todos los  transportistas");
         }
     }
 }
